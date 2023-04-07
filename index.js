b2f796fb81ca6733fa62fac784f04300ff4d8cca
Meacha1 <meachattd@gmail.com>
2023-04-07 23:41:41 +0300

    my first commit

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
+let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 
 
 inputBtn.addEventListener("click", function() {
-    console.log("Button clicked!")});
+    myLeads.push(inputEl.value);
+    inputEl.value = "";
+    renderLeads();
+});
 
+function renderLeads() {
+let listItems = ""
 for (let i = 0; i < myLeads.length; i++) {
-        ulEl.textContent += myLeads[i] + " ";
-    }   
+        listItems += `<li> <a href = '#' target='_blank'> ${myLeads[i]} </a></li>`;
+     }
+    ulEl.innerHTML = listItems;
+}
